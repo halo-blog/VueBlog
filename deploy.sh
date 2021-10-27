2f8a44bfd1f58c13a01416ebc83aab80811d3503
Lanqilu <885240677@qq.com>
2021-10-27 17:47:07 +0800

    修改配置

--- deploy.sh
+++ deploy.sh
@@ -10,21 +10,23 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-echo 'b.xugaoyi.com' > CNAME
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+echo 'blog.halo123.com' > CNAME
+#git push -f git@github.com:<Lanqilu>/<Lanqilu>.github.io.git master
+
+#if [ -z "$GITHUB_TOKEN" ]; then
+#  msg='deploy'
+#  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+#else
+#  msg='来自github actions的自动部署'
+#  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
+#  git config --global user.name "xugaoyi"
+#  git config --global user.email "894072666@qq.com"
+#fi
+#git init
+#git add -A
+#git commit -m "${msg}"
+#git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
@@ -39,5 +41,5 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # git commit -m "${msg}"
 # git push -f $codingUrl master # 推送到coding
 
-cd -
-rm -rf docs/.vuepress/dist
+#cd -
+#rm -rf docs/.vuepress/dist
--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -47,14 +47,14 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'Halo', // 必需
+    link: 'https://github.com/Lanqilu', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    name: 'Evan Xu',
-    slogan: '前端界的小学生',
+    avatar: 'https://img.imgdb.cn/item/608289f3d1a9ae528feb09a8.jpg',
+    name: 'Halo',
+    slogan: '我们每天度过的日常，都是连续不断的奇迹',
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
@@ -63,17 +63,17 @@ module.exports = {
       {
         iconClass: 'icon-youjian',
         title: '发邮件',
-        link: 'mailto:894072666@qq.com',
+        link: 'mailto:885240677@qq.com',
       },
       {
         iconClass: 'icon-github',
         title: 'GitHub',
-        link: 'https://github.com/xugaoyi',
+        link: 'https://github.com/Lanqilu',
       },
       {
         iconClass: 'icon-erji',
         title: '听音乐',
-        link: 'https://music.163.com/#/playlist?id=755597173',
+        link: 'https://music.163.com/#/playlist?id=2156211346',
       },
     ],
   },
@@ -81,7 +81,7 @@ module.exports = {
     // 页脚信息
     createYear: 2019, // 博客创建年份
     copyrightInfo:
-      'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      'Halo | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
